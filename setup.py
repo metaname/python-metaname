3ed4757e52be148db627dfd8dcfd9428b372442e
alzeih <alzeih@gmail.com>
2014-03-27 20:49:03 +1300

    update author_email and requires

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 setup(name='metaname',
       version='0.1.0',
       author='Metaname',
+      author_email='support@metaname.co.nz',
       url='https://test.metaname.net/api/python',
       py_modules=['metaname'],
+      requires=['requests'],
       )
