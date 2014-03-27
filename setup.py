9dfaf19e644fe2a96c3e72b8fe871c3aae414785
alzeih <alzeih@gmail.com>
2014-03-27 20:49:54 +1300

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='metaname',
-      version='0.1.0',
+      version='0.1.1',
       author='Metaname',
       author_email='support@metaname.co.nz',
       url='https://test.metaname.net/api/python',
